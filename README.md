66dd6d5e029e5639763271a66d43f53b5c796836
AndrianaCodes <andriana.amg@gmail.com>
2020-11-10 02:55:58 -0500

    updated app

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # TODO
 * create migrations ---DONE (for now)
-* create models with relationships
+* create models with relationships ---Done (for now)
 * plan out routes for controller following restful conventions while deviating when necessary
 * build controller actions and views simutaneously
+*build views (forms)
+*build navigation
 
 # Source
--- app/controllers/events.controller.rb
+++ app/controllers/events.controller.rb
@@ -4,29 +4,66 @@ class EventsController < ApplicationController
 
         # New 
         #make a get request to '/events/new'
+        get '/events/new' do 
+            erb :'/events/new'
+        end
 
         #Create
         #make a post request to '/events'
+        post '/events' do
+            event = Event.new(params)
+            if !event.title.empty? && !event.date.empty?
+                event.save
+                redirect '/events'
+            else
+                @error = "Data invalid. Please try again."
+                erb :'events/new'
+            end
+        end
 
     #READ
          #index
             #make a get request to '/events'
 
         get '/events' do 
+            @events = Event.all.reverse
             erb :'events/index'
         end
 
         #show
             #make a get request to '/events/:id'
 
+        get '/events/:id' do 
+            @event = Event.find(params[:id])
+            erb:'events/show'
+        end
+
 
     #UPDATE
 
         # Edit
             #make a get reqest to '/events/:id/edit'
+            get '/events/:id/edit' do
+                @event = Event.find(params[:id])
+                erb :'/events/edit'
+            end
 
         #Update
             #make a patch request to '/events/:id'
+            patch '/events/:id' do
+                @event = Event.find(params[:id])
+                if !params["event"]["title"].empty? && !params["event"]["date"].empty?
+                   @event.update(params["event"])
+                    redirect "/events/#{params[:id]}"
+                else
+                    @error = "Data invalid. Please try again."
+                    erb :'events/edit'
+                end
+
+
+                event.update(title: params["title"], date: params["date"], address: params["address"], image: params["image"])
+
+            end
 
     #DESTROY
 
