b4c44e8bca6fda1eaa4a51ea3367eec18fda9183
AndrianaCodes <andriana.amg@gmail.com>
2020-11-11 15:47:12 -0500

    refactored code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,18 +8,13 @@ class UsersController < ApplicationController
     post '/signup' do
 
         user = User.new(params)
-        if user.username.empty? || user.password.empty?
-            @error = "Username and password can't be blamk"
-            erb :'users/signup'
-        elsif User.find_by(username: user.username)
-            @error = "Username already exists"
-            erb :'users/signup'
-        else 
-            user.save
+        if user.save
             session[:user_id] = user.id
             redirect '/events'
+        else 
+            @error = "Invalid credentials"
+            erb :'/users/signup'
         end
-
     end
 
     #users can delete account
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+    validates :username, :password, presence: true 
+    validates_uniqueness_of :username
     has_many :events
-    #testing
 end
